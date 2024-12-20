5d826b766b3d7fbc959c83b1a7a31d8f8e9e8542
yatesjdy <yatesjdy@users.noreply.github.com>
2024-12-20 10:10:43 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+# PLEASE SEE THE ACCOMPANYING README.md File in this REPL for complete instructions on this lab
+  
+# initialize the variables below with the exact values shown in comments to the right of each variable name
+
+# To do this, you must replace all ??? found in the code below the correct values
+# don't forget to put quotes around collections of characters that represents words (like "Sunny" below)
+# note that values we want to do "math" on do not need quotes!
+
+
+wcondition =  "Sunny"   # DONE FOR YOU  Sunny (a string variable)
+temperature=  ???   # 75.54 (a float variable)
+tempUnits=  ???     # degrees (a string variable)
+chanceOfRain=  ???  # 61 (an integer variable)
+percent=  ???       # % (a string variable)
+dayOfMonth=  ???    # 24 (an integer variable)
+daySuffix=  ???     # th (goes after dayOfMonth, a string variable)
+monthName=  ???     # June (a string variable)
+cityName=  ???      # Larkspur (a string variable)
+stateName=  ???     # California (a string variable)
+
+# TODO: print a description of the weather using the variables provided above and the print() function
+# add punctuation and spacing necessary to create a well-written sentence:
+# The weather for the 24 th day of June in Larkspur, California will be Sunny and 75.54 degrees, with a 61% chance of rain.
+# you must use all the VARIABLES defined above in your solution
+# don't forget to use commas to separate arguments within your print statement's parentesis ( and )
+
+print( ??? )
+  
+  
